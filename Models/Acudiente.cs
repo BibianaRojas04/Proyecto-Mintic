c1c4dd7ca6bf189699f2b52df486da5cf4e90130
kmilo8888 <111647014+kmilo8888@users.noreply.github.com>
2022-09-19 20:06:32 -0500

    versionfinal1

---
+++ Models/Acudiente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOOLEANDATA.Models
+{
+    public partial class Acudiente
+    {
+        public Acudiente()
+        {
+            RegistroMatriculas = new HashSet<RegistroMatricula>();
+        }
+
+        public int IdAcudiente { get; set; }
+        public string Dni { get; set; } = null!;
+        public string TipoDocumento { get; set; } = null!;
+        public string NombreApellidos { get; set; } = null!;
+        public DateTime FechaNacimiento { get; set; }
+        public string DireccionCasa { get; set; } = null!;
+        public string? TelefonoCasa { get; set; }
+        public string? TelefonoOpcional { get; set; }
+        public string? Genero { get; set; }
+        public string Email { get; set; } = null!;
+        public string Contraseña { get; set; } = null!;
+        public string Ocupacion { get; set; } = null!;
+        public bool? Parentesco { get; set; }
+        public string DireccionTrabajo { get; set; } = null!;
+
+        public virtual ICollection<RegistroMatricula> RegistroMatriculas { get; set; }
+    }
+}
---
+++ Models/RegistroMatricula.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOOLEANDATA.Models
+{
+    public partial class RegistroMatricula
+    {
+        public int IdMatricual { get; set; }
+        public DateTime? FechaMatricula { get; set; }
+        public double? ValorMatricula { get; set; }
+        public int? IdEstudiante1 { get; set; }
+        public int? IdAcudiente1 { get; set; }
+        public int? IdPersonaAdmi1 { get; set; }
+        public string? Curso { get; set; }
+
+        public virtual Acudiente? IdAcudiente1Navigation { get; set; }
+        public virtual Estudiante? IdEstudiante1Navigation { get; set; }
+        public virtual PersonaAdministrativo? IdPersonaAdmi1Navigation { get; set; }
+    }
+}
